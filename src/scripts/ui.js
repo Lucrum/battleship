2a878b427339a1d3c6584561dd40f03fdb7d7169
Elton <28992495+Lucrum@users.noreply.github.com>
2024-01-19 13:15:32 -0500

    DRY up board generation code Boards can now be clicked

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,42 +1,35 @@
 const playerBoardArea = document.querySelector("div#player-board");
 const enemyBoardArea = document.querySelector("div#enemy-board");
 
-function generatePlayerBoard(gameboard) {
+function generateBoard(gameboard, player, playerClickFunction) {
   const cellList = [];
   for (let i = 0; i < gameboard.dimensions; i += 1) {
     for (let j = 0; j < gameboard.dimensions; j += 1) {
       const cell = document.createElement("div");
-      cell.classList.add(
-        "cell",
-        gameboard.board[i][j] === undefined ? "empty" : "occupied"
-      );
-      cellList.push(cell);
-    }
-  }
-  return cellList;
-}
-
-function generateEnemyBoard(gameboard) {
-  const cellList = [];
-  for (let i = 0; i < gameboard.dimensions; i += 1) {
-    for (let j = 0; j < gameboard.dimensions; j += 1) {
-      const cell = document.createElement("div");
-      cell.classList.add(
-        "cell",
-        gameboard.board[i][j] === undefined ? "empty" : "hit"
-      );
+      cell.classList.add("cell");
+      if (player) {
+        cell.classList.add(
+          gameboard.board[i][j] === undefined ? "empty" : "occupied"
+        );
+      }
       cell.dataset.row = i;
       cell.dataset.col = j;
       cell.textContent = gameboard.board[i][j];
+      cell.addEventListener(() => playerClickFunction(i, j));
       cellList.push(cell);
     }
   }
   return cellList;
 }
 
-export function renderBoards(playerBoard, enemyBoard) {
-  const player = generatePlayerBoard(playerBoard);
-  const enemy = generateEnemyBoard(enemyBoard);
+export function renderBoards(
+  playerBoard,
+  enemyBoard,
+  playerSelfFunction,
+  playerEnemyFunction
+) {
+  const player = generateBoard(playerBoard, true, playerSelfFunction);
+  const enemy = generateBoard(enemyBoard, false, playerEnemyFunction);
 
   playerBoardArea.replaceChildren(...player);
   enemyBoardArea.replaceChildren(...enemy);
